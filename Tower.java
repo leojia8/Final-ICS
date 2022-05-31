3bed3c07063b118dff6acddfc12163c3a27c4b19
leojia8 <leo8.jia@gmail.com>
2022-05-31 12:46:37 -0400

    stuff

--- Tower.java
+++ Tower.java
@@ -1,21 +1,22 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class thing here.
+ * Write a description of class Tower here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class thing extends Actor
+public class Tower extends Actor
 {
     /**
-     * Act - do whatever the thing wants to do. This method is called whenever
+     * Act - do whatever the Tower wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Tower(){
+        getImage().scale(60,60);
+    }
     public void act()
     {
         // Add your action code here.
-        //lalalalal
-        //Zxczxcxzch
     }
 }
