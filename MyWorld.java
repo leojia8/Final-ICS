c33d7e2f7e015647361e77f2689f7e2bd126913e
leojia8 <leo8.jia@gmail.com>
2022-05-31 12:53:53 -0400

    getting rid of benny

--- MyWorld.java
+++ MyWorld.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
- * @author Benny
+ * @author
  * @version (a version number or a date)
  * angus promises to do all the work
  */
