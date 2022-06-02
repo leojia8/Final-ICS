23e1b0fa24a35f3519233af46c25ccffc2de3972
CelebSev <106546561+CelebSev@users.noreply.github.com>
2022-06-02 12:17:51 -0400

    asdfasdlkjkflsadjl;fjlasdjf

---
+++ LoseWorld.java
@@ -0,0 +1,22 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class LoseWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LoseWorld extends World
+{
+    private GreenfootImage image;
+
+    public LoseWorld()
+    {
+        
+        super(1200, 800, 1);
+        
+        
+    }
+
+    
+}
---
+++ WinWorld.java
@@ -0,0 +1,22 @@
+/**
+ * Write a description of class WinWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import greenfoot.*;
+public class WinWorld extends World
+{
+   
+    /**
+     * Constructor for objects of class WinWorld
+     */
+    public WinWorld()
+    {
+        
+        super(1200, 800, 1);
+    }
+
+  
+}
