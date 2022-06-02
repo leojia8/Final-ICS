0bbfff5a006c4980c203a78ce20f7fdd5dee1eca
CelebSev <106546561+CelebSev@users.noreply.github.com>
2022-06-01 20:48:31 -0400

    a

---
+++ TowerButton.java
@@ -0,0 +1,29 @@
+import greenfoot.*;
+
+
+/**
+ * Write a description of class TowerButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TowerButton extends Actor 
+{
+    
+    
+    public TowerButton()
+    {
+        
+        
+    }
+    
+    
+
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(this))
+        {
+            
+        }
+    }
+}
