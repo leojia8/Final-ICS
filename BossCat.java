898710696d3a4cf1c0cb8d41e9e34e17c86f6ec3
CelebSev <106546561+CelebSev@users.noreply.github.com>
2022-06-03 12:54:52 -0400

    c

--- BossCat.java
+++ BossCat.java
@@ -11,40 +11,60 @@ public class BossCat extends Cat
     private GreenfootImage image;
     private int speed;
     private boolean reverse; 
+    private static final int HP = 1200;
+    private int counter;
+    private boolean justSpawned;
     /**
      * Constructor for objects of class BossCat
      */
     public BossCat()
     {
-        
-        
-        
+        hpBar = new StatBar(HP, HP, this, 30, 5, 60, Color.RED, new Color(255, 204, 203), false, Color.WHITE, 1);
+        reverse = true;
+        image = new GreenfootImage("normalcat.png");
+        image.scale(64,64);
+        setImage(image);
+        counter = 15;
+        justSpawned = true;
     }
-    
-    
+
     public void act()
     {
-        if(speed == 5)
-        {
-            speed--;
-            setLocation(getX(), getY() + speed);
-        }
-        else if(speed == -5)
-        {
-            speed++;
-            setLocation(getX(), getY() + speed);
-        }
-       
         
-        
-    
+        if(justSpawned)
+        {
+            
+        }
+        counter--;
+        if(counter <= 0)
+        {
+            
+            if(reverse)
+            {
+
+                speed--;
+                setLocation(getX(), getY() + speed);
+                if(speed == -5)
+                {
+                    reverse = false;
+                }
+            }
+            else if(reverse == false)
+            {
+                speed++;
+                setLocation(getX(), getY() + speed);
+                if(speed == 5)
+                {
+                    reverse = true;
+                }
+            }
+            counter = 15;
+        }
+
     
     
     }
 
-    
-    
-    
     
     
 }
---
+++ BossRing.java
@@ -0,0 +1,30 @@
+/**
+ * Write a description of class BossRing here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BossRing  
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class BossRing
+     */
+    public BossRing()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
