fedc8a1bc0182ffb069a155b8d6b189c8cb1c4e1
CelebSev <106546561+CelebSev@users.noreply.github.com>
2022-06-04 17:53:35 -0400

    changed cat

--- Cat.java
+++ Cat.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public abstract class Cat extends Actor
+public abstract class Cat extends SuperSmoothMover
 {
     protected int health;
     protected int speed;
