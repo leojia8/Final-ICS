f4ec68242fd63ba328d2890473d917c702a6e575
leojia8 <leo8.jia@gmail.com>
2022-06-23 18:24:47 -0400

    changed counter bar to white (old code is still there)

--- Counter.java
+++ Counter.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * The Counter Bar: The Blue Bar in the top middle of the screen which displays the amount of resources the player has.
+ * The Counter Bar: The White Bar in the top middle of the screen which displays the amount of resources the player has.
  * 
  * @author Thomas Yang, Angus Feng
  * @version June 2022
@@ -14,13 +14,14 @@ public class Counter extends Actor
      * Sets the image to text displaying the number of resources.
      */
     public Counter(){
-        setImage(new GreenfootImage("Stone: "+ w.getStone() + "     Wood: " + w.getWood() + "     Metal: " + w.getMetal() + "        Dog Food: " + w.getCatFood() ,40, Color.BLACK, new Color(0, 176, 225)));
+        setImage(new GreenfootImage("Stone: "+ w.getStone() + "     Wood: " + w.getWood() + "     Metal: " + w.getMetal() + "        Cat Food: " + w.getCatFood() ,40, Color.BLACK, new Color(255, 255, 255)));
+        //setImage(new GreenfootImage("Stone: "+ w.getStone() + "     Wood: " + w.getWood() + "     Metal: " + w.getMetal() + "        Cat Food: " + w.getCatFood() ,40, Color.BLACK, new Color(0, 176, 225)));
     }
     /**
      * Updates the counter bar.
      */
     public void act()
     {
-        setImage(new GreenfootImage("Stone: "+ w.getStone() + "     Wood: " + w.getWood() + "     Metal: " + w.getMetal() + "        Dog Food: " + w.getCatFood(),40, Color.BLACK, new Color(0, 176, 225)));
+        setImage(new GreenfootImage("Stone: "+ w.getStone() + "     Wood: " + w.getWood() + "     Metal: " + w.getMetal() + "        Cat Food: " + w.getCatFood(),40, Color.BLACK, new Color(0, 176, 225)));
     }
 }
