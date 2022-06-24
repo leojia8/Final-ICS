ed10823e923430e258271e0ef9d5488af348702b
leojia8 <leo8.jia@gmail.com>
2022-06-23 21:40:22 -0400

    instruction world and story world buttons

---
+++ InstructionsWorld.java
@@ -0,0 +1,39 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * An instruction menu telling the user what to do.
+ * 
+ * @author Leo Jia
+ * @version June 2022
+ */
+public class InstructionsWorld extends World
+{
+    private GreenfootImage image;
+    private TextButton next = new TextButton("Next", 100, Color.BLACK, Color.WHITE);
+    /**
+     * Sets and scales the instructions image. Adds a button to allow the user to go to the game world.
+     * 
+     */
+    public InstructionsWorld()
+    {    
+        super(1200, 800, 1); 
+        
+        image = new GreenfootImage("instructionsworld.png");
+        image.scale(1200, 800);
+        setBackground(image);
+        
+        addObject(next, 1110, 750);
+    }
+    
+    /**
+     * Sends the user to the game world when the next button is clicked.
+     */
+    public void act()
+    {
+        
+        if (Greenfoot.mouseClicked(next)){
+            GameWorld gameWorld = new GameWorld();
+            Greenfoot.setWorld(gameWorld);
+        } 
+    }
+}
--- StoryWorld.java
+++ StoryWorld.java
@@ -1,15 +1,18 @@
 
 import greenfoot.*;
 /**
- * A story menu for the simulation. Tells users the story of the game, as well as how to play.
+ * A story menu for the simulation. Tells users the story of the game.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Leo Jia 
+ * @version June 2022
  */
 public class StoryWorld extends World
 {
     private GreenfootImage image;
-    
+    private TextButton next = new TextButton("Next", 100, Color.BLACK, Color.WHITE);
+    /**
+     * Sets and scales the story image. Adds a button to allow the user to go to the instructions world
+     */
     public StoryWorld() 
     {
         super(1200, 800, 1);
@@ -17,17 +20,21 @@ public class StoryWorld extends World
         image = new GreenfootImage("storyworldtext.png");
         image.scale(1200, 800);
         setBackground(image);
+        
+        addObject(next, 1110, 750);
     }
-
+    
+    /**
+     * Sends the user to the instructions world when the next button is clicked.
+     */
     public void act()
     {
         
+        if (Greenfoot.mouseClicked(next)){
+            InstructionsWorld instructionsWorld = new InstructionsWorld();
+            Greenfoot.setWorld(instructionsWorld);
+        } 
     }
-    
-    
-    
-    
-    
     
     
     
